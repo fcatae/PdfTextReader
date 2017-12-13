0f7d464b66e3c3a2c7eef964e99947207d5a084b
Fabricio Catae <fcatae@microsoft.com>
2017-12-13 11:15:33 -0200

    Add project files

---
+++ src/PdfTextReader/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PdfTextReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var user = new UserWriter();
+            user.Process("bin/p40.pdf", "bin/output.pdf");
+        }
+    }
+}
---
+++ src/PdfTextReader/UserWriter.cs
@@ -0,0 +1,27 @@
+﻿using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas;
+using iText.Layout;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace PdfTextReader
+{
+    class UserWriter
+    {
+        public void Process(string srcpath, string dstpath)
+        {
+            using (var pdf = new PdfDocument(new PdfReader(srcpath), new PdfWriter(dstpath)))
+            {
+                //var document = new Document(pdf);
+                var page = pdf.GetPage(1);
+
+                var canvas = new PdfCanvas(page);
+
+                canvas.Rectangle(10, 10, 100, 100);
+                canvas.Stroke();
+            }
+        }
+    }
+}
