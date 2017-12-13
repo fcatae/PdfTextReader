02be7440c3423ddd075167be3164544d1ecd9c26
Fabricio Catae <fcatae@microsoft.com>
2017-12-13 16:09:56 -0200

    add blockset

---
+++ src/PdfTextReader/BlockSet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using iText.Kernel.Geom;
+
+namespace PdfTextReader
+{
+    class BlockSet
+    {
+        List<Block> _list = new List<Block>();
+
+        public void Add(Block block)
+        {
+            _list.Add(block);
+        }
+
+        public float GetX() => _list.Min(b => b.X);
+        public float GetH() => _list.Min(b => b.H);
+        public float GetWidth() => _list.Max(b => b.X + b.Width) - _list.Min(b => b.X);
+        public float GetHeight() => _list.Max(b => b.H + b.Height) - _list.Min(b => b.H);
+
+        public float Update()
+        {
+            float minX = _list.Min(b => b.X);
+            float maxX = _list.Max(b => b.X + b.Width);
+            float minH = _list.Min(b => b.H);
+            float maxH = _list.Max(b => b.H + b.Height);
+
+            return minX;
+        }
+
+    }
+}
--- src/PdfTextReader/UserWriter.cs
+++ src/PdfTextReader/UserWriter.cs
@@ -44,19 +44,48 @@ namespace PdfTextReader
             {
                 var page = pdf.GetPage(1);
                 var canvas = new PdfCanvas(page);
+                var tmp = new BlockSet();
+                int count = 0;
 
                 var parser = new PdfCanvasProcessor(new UserListener( b => {
 
-                    // letra maiuscula: if(b.Text.Length > 0 &&  (b.Text.ToUpper()[0] == b.Text[0]) )
-                    canvas.SetStrokeColor(ColorConstants.YELLOW);
-                    canvas.Rectangle(b.X, b.B, b.Width, b.Height - b.Lower);
-                    canvas.Stroke();
+                    count++;
+
+                    tmp.Add(b);
+
+                    if(count > 20)
+                    {
+                        var ttmp = new
+                        {
+                            a = tmp.GetX(),
+                            b = tmp.GetH(),
+                            c = tmp.GetWidth(),
+                            d = tmp.GetHeight()
+                        };
+
+                        canvas.SetStrokeColor(ColorConstants.YELLOW);
+                        canvas.Rectangle(tmp.GetX(), tmp.GetH(), tmp.GetWidth(), tmp.GetHeight());
+                        canvas.Stroke();
+
+                        // reset
+                        tmp = new BlockSet();
+                        count = 0;
+                    }
 
                 }));
 
                 parser.ProcessPageContent(page);
             }
         }
+
+        public void ExampleMarker(Block b, PdfCanvas canvas)
+        {
+            // letra maiuscula: if(b.Text.Length > 0 &&  (b.Text.ToUpper()[0] == b.Text[0]) )
+            canvas.SetStrokeColor(ColorConstants.YELLOW);
+            canvas.Rectangle(b.X, b.B, b.Width, b.Height - b.Lower);
+            canvas.Stroke();
+        }
+
         public void ProcessMarker2(string srcpath, string dstpath)
         {
             using (var pdf = new PdfDocument(new PdfReader(srcpath), new PdfWriter(dstpath)))
