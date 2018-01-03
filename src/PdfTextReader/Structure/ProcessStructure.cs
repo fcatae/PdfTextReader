b21273d729343617aac202b157ea9f2cef4e5c92
Lucas Humenhuk <lucas.decarli@outlook.com>
2018-01-03 15:13:08 -0200

    Processing Paragraph Method under Construction

--- src/PdfTextReader/Structure/ProcessStructure.cs
+++ src/PdfTextReader/Structure/ProcessStructure.cs
@@ -7,22 +7,12 @@ using PdfTextReader.Lucas_Testes.Helpers;
 
 namespace PdfTextReader.Structure
 {
-    public static class ProcessStructure
+    class ProcessStructure
     {
 
-        public static void ProcessLine(List<MainItem> items)
+        public List<TextItem> ProcessLine(BlockSet items)
         {
-            foreach (MainItem item in items)
-            {
-                TextLine line = new TextLine();
-                if (items[0].GetType() == typeof(TextItem))
-                {
-                    var i = items[0] as TextItem;
-
-                    line.FontName =  i.fontName;
-                    line.FontSize = Decimal.Round(Convert.ToDecimal(i.fontSize), 2);
-                }
-            }
+            throw new InvalidOperationException();
         }
     }
 }
---
+++ src/PdfTextReader/Structure/ProcessStructure2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PdfTextReader.Structure
+{
+    class ProcessStructure2
+    {
+        public List<TextStructure> ProcessParagraph(List<TextLine> lineSet)
+        {
+            throw new InvalidOperationException(); 
+        }
+    }
+}
