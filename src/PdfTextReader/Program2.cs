50998a47ef5983be1a292d8d526b7efc1c2b07f7
Fabricio Catae <fcatae@microsoft.com>
2018-01-04 11:37:27 -0200

    remove pipeline from program2

--- src/PdfTextReader/Program2.cs
+++ src/PdfTextReader/Program2.cs
@@ -68,37 +68,6 @@ namespace PdfTextReader
         {
             Stats.ProcessStats.PrintAnalytics(pdfname, lines, structures, contents);
         }
-
-        void TestPipeline()
-        {
-            var pipeline = new Execution.Pipeline();
-
-            pipeline.Input("input") //.Output
-                    .Page(1)  // .AllPages( p => p.CurrentPage )
-                    .ParsePdf<PreProcessTables>()
-                    .StoreResult("INLINETABLES")
-                        .Output("table-output")
-                        .Show<TableCell>(b => b.Op == 1, Color.Green)
-                    .Output("lines")
-                    .ParsePdf<ProcessPdfText>()
-                    .ParseBlock<RemoveTableInlineText>() // "INLINETABLES"
-                                                         //.ParseBlock<PreProcessTables>() // use singleton instead?
-                    .ParseBlock<FindPageColumns>()
-                    .ParseBlock<BreakColumns>()
-                        .Validate<BreakColumns>(Color.Red)
-                    .ParseBlock<RemoveHeader>().Debug(Color.Blue)
-                    .ParseBlock<RemoveFooter>().Debug(Color.Blue)
-                        .Validate<ValidFooter>(p => new Exception())
-                    .ParseBlock<CreateLines>()
-                    .Text<CreateStructures>()
-                        .Show(Color.Yellow)
-                    .ParseText<ProcessParagraphs>()
-                    .ParseText<ProcessStructure>()
-                        .Output("structures")
-                        .Show(Color.Red)
-                    .ParseContent<ProcessArticle>()
-                        ;//.SaveXml(p => $"file-{p.page}");
-
-        }
+                
     }
 }
