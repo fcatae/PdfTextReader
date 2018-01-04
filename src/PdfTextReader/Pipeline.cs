83bb0d0f5f745ff443c99532e784ca2dd42058ca
Fabricio Catae <fcatae@microsoft.com>
2018-01-04 08:36:07 -0200

    move code to pipeline

--- src/PdfTextReader/Pipeline.cs
+++ src/PdfTextReader/Pipeline.cs
@@ -1,12 +1,46 @@
 ﻿using PdfTextReader.Execution;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace PdfTextReader
 {
     class Pipeline
-    {        
+    {
+
+        void TestPipeline()
+        {
+            var pipeline = new Execution.Pipeline();
+
+            pipeline.Input("input") //.Output
+                    .Page(1)  // .AllPages( p => p.CurrentPage )
+                    .ParsePdf<PreProcessTables>()
+                    .StoreResult("INLINETABLES")
+                        .Output("table-output")
+                        .Show<TableCell>(b => b.Op == 1, Color.Green)
+                    .Output("lines")
+                    .ParsePdf<ProcessPdfText>()
+                    .ParseBlock<RemoveTableInlineText>() // "INLINETABLES"
+                                                         //.ParseBlock<PreProcessTables>() // use singleton instead?
+                    .ParseBlock<FindPageColumns>()
+                    .ParseBlock<BreakColumns>()
+                        .Validate<BreakColumns>(Color.Red)
+                    .ParseBlock<RemoveHeader>().Debug(Color.Blue)
+                    .ParseBlock<RemoveFooter>().Debug(Color.Blue)
+                        .Validate<ValidFooter>(p => new Exception())
+                    .ParseBlock<CreateLines>()
+                    .Text<CreateStructures>()
+                        .Show(Color.Yellow)
+                    .ParseText<ProcessParagraphs>()
+                    .ParseText<ProcessStructure>()
+                        .Output("structures")
+                        .Show(Color.Red)
+                    .ParseContent<ProcessArticle>()
+                        ;//.SaveXml(p => $"file-{p.page}");
+
+        }
+
         // Pipeline Definition: 
         //   * TableListener -> ProcessTable
         //   * TextListener  -> Remove Tables -> ProcessColumns -> BreakColumns -> MergeBlocks
