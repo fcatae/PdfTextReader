44b4f346a9edf4fc235fa2e922a3cee96f438cb2
Fabricio Catae <fcatae@microsoft.com>
2018-01-11 15:21:59 -0200

    remove PrintAnalytics 1 and 2

--- src/PdfTextReader/ExecutionStats/PrintAnalytics.cs
+++ src/PdfTextReader/ExecutionStats/PrintAnalytics.cs
@@ -1,127 +1,127 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using PdfTextReader.Base;
-using PdfTextReader.Parser;
-using System.IO;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using PdfTextReader.Base;
+//using PdfTextReader.Parser;
+//using System.IO;
 
-namespace PdfTextReader.ExecutionStats
-{
-    class PrintAnalytics : IProcessStructure<TextLine>, IProcessStructure<TextStructure>, IProcessStructure<Conteudo>, IDisposable
-    {
-        StreamWriter file;
+//namespace PdfTextReader.ExecutionStats
+//{
+//    class PrintAnalytics : IProcessStructure<TextLine>, IProcessStructure<TextStructure>, IProcessStructure<Conteudo>, IDisposable
+//    {
+//        StreamWriter file;
 
-        public static IProcessStructure<TextLine> ShowTextLine(string pdfname)
-        {
-            var analytics = new PrintAnalytics(pdfname);
+//        public static IProcessStructure<TextLine> ShowTextLine(string pdfname)
+//        {
+//            var analytics = new PrintAnalytics(pdfname);
 
-            analytics.WriteLine("<<<<<>>>>>>>>>>>>>");
-            analytics.WriteLine("PROCESSING LINES");
-            analytics.WriteLine("<<<<<>>>>>>>>>>>>>");
-            analytics.WriteLine("");
-            analytics.WriteLine("");
+//            analytics.WriteLine("<<<<<>>>>>>>>>>>>>");
+//            analytics.WriteLine("PROCESSING LINES");
+//            analytics.WriteLine("<<<<<>>>>>>>>>>>>>");
+//            analytics.WriteLine("");
+//            analytics.WriteLine("");
 
-            return analytics;
-        }
+//            return analytics;
+//        }
 
-        public static IProcessStructure<TextStructure> ShowTextStructure(string pdfname)
-        {
-            var analytics = new PrintAnalytics(pdfname);
+//        public static IProcessStructure<TextStructure> ShowTextStructure(string pdfname)
+//        {
+//            var analytics = new PrintAnalytics(pdfname);
 
-            analytics.WriteLine("<<<<<>>>>>>>>>>>>>");
-            analytics.WriteLine("PROCESSING PARAGRAPHS");
-            analytics.WriteLine("<<<<<>>>>>>>>>>>>>");
-            analytics.WriteLine("");
-            analytics.WriteLine("");
+//            analytics.WriteLine("<<<<<>>>>>>>>>>>>>");
+//            analytics.WriteLine("PROCESSING PARAGRAPHS");
+//            analytics.WriteLine("<<<<<>>>>>>>>>>>>>");
+//            analytics.WriteLine("");
+//            analytics.WriteLine("");
 
-            return analytics;
-        }
+//            return analytics;
+//        }
 
-        public static IProcessStructure<Conteudo> ShowConteudo(string pdfname)
-        {
-            var analytics = new PrintAnalytics(pdfname);
+//        public static IProcessStructure<Conteudo> ShowConteudo(string pdfname)
+//        {
+//            var analytics = new PrintAnalytics(pdfname);
 
-            analytics.WriteLine("<<<<<>>>>>>>>>>>>>");
-            analytics.WriteLine("PROCESSING CONTENTS");
-            analytics.WriteLine("<<<<<>>>>>>>>>>>>>");
-            analytics.WriteLine("");
-            analytics.WriteLine("");
+//            analytics.WriteLine("<<<<<>>>>>>>>>>>>>");
+//            analytics.WriteLine("PROCESSING CONTENTS");
+//            analytics.WriteLine("<<<<<>>>>>>>>>>>>>");
+//            analytics.WriteLine("");
+//            analytics.WriteLine("");
 
-            return analytics;
-        }
+//            return analytics;
+//        }
 
-        private PrintAnalytics(string pdfname)
-        {
-            this.file = new StreamWriter($"{pdfname}-Analytics.txt");
+//        private PrintAnalytics(string pdfname)
+//        {
+//            this.file = new StreamWriter($"{pdfname}-Analytics.txt");
 
-            file.WriteLine("<<<<<>>>>>>>>>>>>>");
-            file.WriteLine("START ANALYTICS");
-            file.WriteLine("<<<<<>>>>>>>>>>>>>");
-            file.WriteLine("");
-            file.WriteLine("");
-        }
+//            file.WriteLine("<<<<<>>>>>>>>>>>>>");
+//            file.WriteLine("START ANALYTICS");
+//            file.WriteLine("<<<<<>>>>>>>>>>>>>");
+//            file.WriteLine("");
+//            file.WriteLine("");
+//        }
 
-        private void WriteLine(string text)
-        {
-            file.WriteLine(text);
-        }
+//        private void WriteLine(string text)
+//        {
+//            file.WriteLine(text);
+//        }
 
-        public TextLine Process(TextLine line)
-        {
-            file.WriteLine($"Breakline: {line.Breakline}");
-            file.WriteLine($"FontName: {line.FontName}");
-            file.WriteLine($"FontSize: {line.FontSize}");
-            file.WriteLine($"FontStyle: {line.FontStyle}");
-            file.WriteLine($"MarginLeft: {line.MarginLeft}");
-            file.WriteLine($"MarginRight: {line.MarginRight}");
-            file.WriteLine($"Text: {line.Text}");
-            file.WriteLine("");
-            file.WriteLine("-----");
+//        public TextLine Process(TextLine line)
+//        {
+//            file.WriteLine($"Breakline: {line.Breakline}");
+//            file.WriteLine($"FontName: {line.FontName}");
+//            file.WriteLine($"FontSize: {line.FontSize}");
+//            file.WriteLine($"FontStyle: {line.FontStyle}");
+//            file.WriteLine($"MarginLeft: {line.MarginLeft}");
+//            file.WriteLine($"MarginRight: {line.MarginRight}");
+//            file.WriteLine($"Text: {line.Text}");
+//            file.WriteLine("");
+//            file.WriteLine("-----");
 
-            return line;
-        }
+//            return line;
+//        }
 
-        public TextStructure Process(TextStructure structure)
-        {
-            file.WriteLine($"TextAlignment: {structure.TextAlignment}");
-            file.WriteLine($"FontName: {structure.FontName}");
-            file.WriteLine($"FontSize: {structure.FontSize}");
-            file.WriteLine($"FontStyle: {structure.FontStyle}");
-            file.WriteLine($"Text: {structure.Text}");
-            file.WriteLine("");
-            file.WriteLine("-----");
+//        public TextStructure Process(TextStructure structure)
+//        {
+//            file.WriteLine($"TextAlignment: {structure.TextAlignment}");
+//            file.WriteLine($"FontName: {structure.FontName}");
+//            file.WriteLine($"FontSize: {structure.FontSize}");
+//            file.WriteLine($"FontStyle: {structure.FontStyle}");
+//            file.WriteLine($"Text: {structure.Text}");
+//            file.WriteLine("");
+//            file.WriteLine("-----");
 
-            return structure;
-        }
+//            return structure;
+//        }
 
-        public Conteudo Process(Conteudo content)
-        {
-            file.WriteLine($"TextAlignment: {content.TextAlignment}");
-            file.WriteLine($"FontName: {content.FontName}");
-            file.WriteLine($"FontSize: {content.FontSize}");
-            file.WriteLine($"FontStyle: {content.FontStyle}");
-            file.WriteLine($"Text: {content.Text}");
-            file.WriteLine($"Content-Type: {content.ContentType}");
-            file.WriteLine("");
-            file.WriteLine("-----");
+//        public Conteudo Process(Conteudo content)
+//        {
+//            file.WriteLine($"TextAlignment: {content.TextAlignment}");
+//            file.WriteLine($"FontName: {content.FontName}");
+//            file.WriteLine($"FontSize: {content.FontSize}");
+//            file.WriteLine($"FontStyle: {content.FontStyle}");
+//            file.WriteLine($"Text: {content.Text}");
+//            file.WriteLine($"Content-Type: {content.ContentType}");
+//            file.WriteLine("");
+//            file.WriteLine("-----");
 
-            return content;
-        }
+//            return content;
+//        }
         
-        public void Dispose()
-        {
-            if( file != null )
-            {
-                file.WriteLine("");
-                file.WriteLine("");
+//        public void Dispose()
+//        {
+//            if( file != null )
+//            {
+//                file.WriteLine("");
+//                file.WriteLine("");
 
-                file.WriteLine("<<<<<>>>>>>>>>>>>>");
-                file.WriteLine("END ANALYTICS");
-                file.WriteLine("<<<<<>>>>>>>>>>>>>");
+//                file.WriteLine("<<<<<>>>>>>>>>>>>>");
+//                file.WriteLine("END ANALYTICS");
+//                file.WriteLine("<<<<<>>>>>>>>>>>>>");
 
-                file.Dispose();
-                file = null;
-            }
-        }
-    }
-}
+//                file.Dispose();
+//                file = null;
+//            }
+//        }
+//    }
+//}
