fbc0e396393bfa83f83175633c6ea578dbf7a205
Lucas Humenhuk <lucas.decarli@outlook.com>
2018-01-17 07:33:55 -0200

    Calculating Elapsed time

--- src/PdfTextReader/Program.cs
+++ src/PdfTextReader/Program.cs
@@ -9,12 +9,21 @@ namespace PdfTextReader
         static void Main(string[] args)
         {
             Console.WriteLine("PDF Text Reader");
+            var watch = System.Diagnostics.Stopwatch.StartNew();
 
             //Program3.ProcessStats();
 
             Program4.ProcessStats("Done/2013-1");
 
             //Program2.MainTest();
+
+
+            watch.Stop();
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            Console.WriteLine($"Elapsed time was: {elapsedMs}");
+
+            Console.ReadKey();
         }
     }
 }
