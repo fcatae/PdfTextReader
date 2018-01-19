dfc9890bec418e943341d497c84a19bc3fd84034
Fabricio Catae <fcatae@microsoft.com>
2018-01-19 10:17:54 -0200

    add exception handling framework

---
+++ src/PdfTextReader/Base/PdfReaderException.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace PdfTextReader.Base
+{
+    class PdfReaderException : Exception
+    {
+        public PdfReaderException(string message) : base(message)
+        {
+        }
+
+        private static bool g_ContinueOnException = false;
+
+        public static void ContinueOnException()
+        {
+            g_ContinueOnException = true;
+        }
+
+        public static void Throw(string message, string details, [CallerMemberName]string source = null)
+        {
+            try
+            {
+                throw new PdfReaderException(message);
+            }
+            catch(Exception ex)
+            {
+                if (g_ContinueOnException)
+                {
+                    Console.WriteLine("=======================================");
+                    Console.WriteLine($"CRITICAL EXCEPTION: {source} ({message})");
+                    Console.WriteLine();
+                    Console.WriteLine($"    {details}");
+                    Console.WriteLine();
+                    Console.WriteLine("=======================================");
+                    Console.WriteLine(ex.StackTrace);
+                    return;
+                }
+            }
+        }
+    }
+}
