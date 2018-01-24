dbebeb3bfb33af6eeef6157150d88f99ae4a931f
Daniel Semedo <daniel.semedo@outlook.com>
2018-01-24 15:17:59 -0200

    analyze signatures text lines

---
+++ src/PdfTextReader/TextStructures/AnalyzeLinesCenterRight.cs
@@ -0,0 +1,47 @@
+﻿using PdfTextReader.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace PdfTextReader.TextStructures
+{
+    class AnalyzeLinesCenterRight : ILogStructure<TextLine>
+    {
+        public void StartLog(TextWriter input)
+        {
+        }
+
+        public void Log(TextWriter input, TextLine line)
+        {
+           
+
+            float? afterSpace = line.AfterSpace;
+            float? beforeSpace = line.BeforeSpace;
+
+
+            float? lineCenter = (float)line.CenteredAt;
+
+            if (lineCenter < 29 && lineCenter > 28)
+            {
+                input.WriteLine("-----------------------------------");
+                input.WriteLine($"Margins: (LEFT: {line.MarginLeft}, RIGHT: {line.MarginRight}, CENTER: {line.CenteredAt})");
+
+                input.Write($"TEXT: {line.Text}");
+                input.WriteLine($" ({line.FontName}, {line.FontSize.ToString("0.00")}, {line.FontStyle})");
+                input.WriteLine($" (AfterSpace: {afterSpace})");
+                input.WriteLine($" (BeforeSpace: {afterSpace})");
+                input.WriteLine();
+
+                input.WriteLine("");
+
+
+            }
+    
+        }
+
+        public void EndLog(TextWriter input)
+        {
+        }
+    }
+}
