18c66d61b183882c20d4535527517cc57b5e3f32
Fabricio Catae <fcatae@microsoft.com>
2018-01-25 19:39:09 -0200

    test issue #51

--- src/PdfTextReader/Program.cs
+++ src/PdfTextReader/Program.cs
@@ -11,7 +11,7 @@ namespace PdfTextReader
             Console.WriteLine("PDF Text Reader");
             var watch = System.Diagnostics.Stopwatch.StartNew();
 
-            Program3.ProcessStats2(@"DO1_2016_01_13");
+            Program3.ProcessStats2(@"DO1_2016_01_08");
 
             watch.Stop();
             var elapsedMs = watch.ElapsedMilliseconds;
