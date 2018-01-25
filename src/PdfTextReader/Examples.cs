c92283f17f88be9139a14d5f782974297a59e613
Fabricio Catae <fcatae@microsoft.com>
2018-01-25 00:13:51 -0200

    add example #23

--- src/PdfTextReader/Examples.cs
+++ src/PdfTextReader/Examples.cs
@@ -193,12 +193,17 @@ namespace PdfTextReader
                     .AllPagesExcept<CreateTextLines>(new int[] { }, page =>
                               page.ParsePdf<PreProcessTables>()
                                   .ParseBlock<IdentifyTables>()
+                              //.Show(Color.Blue)
                               .ParsePdf<PreProcessImages>()
+                                  .ParseBlock<BasicFirstPageStats>()
+                                  //.Validate<RemoveOverlapedImages>().ShowErrors(p => p.Show(Color.Blue))
                                   .ParseBlock<RemoveOverlapedImages>()
                               .ParsePdf<ProcessPdfText>()
                                   .Validate<RemoveSmallFonts>().ShowErrors(p => p.ShowText(Color.Green))
                                   .ParseBlock<RemoveSmallFonts>()
+                                  //.Validate<MergeTableText>().ShowErrors(p => p.Show(Color.Blue))
                                   .ParseBlock<MergeTableText>()
+                                  //.Validate<HighlightTextTable>().ShowErrors(p => p.Show(Color.Green))
                                   .ParseBlock<HighlightTextTable>()
                                   .ParseBlock<RemoveTableText>()
                                   .ParseBlock<ReplaceCharacters>()
@@ -210,22 +215,36 @@ namespace PdfTextReader
                                   .ParseBlock<FindInitialBlocksetWithRewind>()
                                       .Show(Color.Gray)
                                   .ParseBlock<BreakColumnsLight>()
-                                        //.ParseBlock<BreakColumns>()
+                                  //.ParseBlock<BreakColumns>()
                                   .ParseBlock<AddTableSpace>()
                                   .ParseBlock<RemoveTableOverImage>()
                                   .ParseBlock<RemoveImageTexts>()
                                   .ParseBlock<AddImageSpace>()
                                       .Validate<RemoveFooter>().ShowErrors(p => p.Show(Color.Purple))
                                       .ParseBlock<RemoveFooter>()
+                                  .ParseBlock<AddTableLines>()
+
+                                      // Try to rewrite column
+                                      .ParseBlock<BreakColumnsRewrite>()
+
                                   .ParseBlock<BreakInlineElements>()
                                   .ParseBlock<ResizeBlocksets>()
-                                      .Validate<ResizeBlocksets>().ShowErrors(p => p.Show(Color.Gray))
-                                  .ParseBlock<OrderBlocksets>()
-                                  .Show(Color.Orange)
-                                  .ShowLine(Color.Black)
+                                  .ParseBlock<ResizeBlocksetMagins>()
+
+                                    // Reorder the blocks
+                                    .ParseBlock<OrderBlocksets>()
+
                                   .ParseBlock<OrganizePageLayout>()
+                                  .ParseBlock<MergeSequentialLayout>()
+                                  .ParseBlock<ResizeSequentialLayout>()
+
+                                      .Show(Color.Orange)
+                                      .ShowLine(Color.Black)
+
                                   .ParseBlock<CheckOverlap>()
-                                  .Validate<ValidatePositiveCoordinates>().ShowErrors(p => p.Show(Color.Red))
+
+                                      .Validate<CheckOverlap>().ShowErrors(p => p.Show(Color.Red))
+                                      .Validate<ValidatePositiveCoordinates>().ShowErrors(p => p.Show(Color.Red))
                     );
 
             return result;
