51658196993c329663f290bfb954a2c0a2ad93e0
Fabricio Catae <fcatae@microsoft.com>
2018-01-26 15:02:55 -0200

    add virtual XML

--- src/PdfTextReader/VirtualFS.cs
+++ src/PdfTextReader/VirtualFS.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
 
 namespace PdfTextReader.Base
@@ -38,6 +39,52 @@ namespace PdfTextReader.Base
         public static System.Xml.XmlWriter OpenXmlWriter(string finalURL, System.Xml.XmlWriterSettings settings)
         {
             return System.Xml.XmlWriter.Create($"{finalURL}.xml", settings);
+        }
+
+        public static string GetDirectoryName(string folder)
+        {
+            return new DirectoryInfo(folder).Name;
+        }
+
+        public static string GetDirectoryCreateDirectory(string folder)
+        {
+            return Directory.CreateDirectory(folder).FullName;
+        }
+
+        public static IEnumerable<VFileInfo> DirectoryInfoEnumerateFiles(string folder, string pattern)
+        {
+            var directory = new DirectoryInfo(folder);
+
+            var enumFiles = directory.EnumerateFiles("*.xml");
+
+            return enumFiles.Select(f => new VFileInfo(f));
+        }
+
+        public static void FileWriteAllText(string filename, string text)
+        {
+            File.WriteAllText(filename, text);
+        }
+
+        public static System.Xml.Linq.XDocument XDocumentLoad(string filename)
+        {
+            return System.Xml.Linq.XDocument.Load(filename);
+        }
+
+        public class VFileInfo
+        {
+            FileInfo _f;
+
+            public VFileInfo(FileInfo f)
+            {
+                _f = f;
+            }
+
+            public string Name => _f.Name;
+            public string FullName => _f.FullName;
+            public void CopyTo(string dest)
+            {
+                _f.CopyTo(dest);
+            }
         }
     }
 }
