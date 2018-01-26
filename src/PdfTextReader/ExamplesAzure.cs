6ee86a1afd3ac9400f7b16be6d9b77b436cf8f63
Fabricio Catae <fcatae@microsoft.com>
2018-01-26 15:37:43 -0200

    add examples azure

---
+++ src/PdfTextReader/ExamplesAzure.cs
@@ -0,0 +1,26 @@
+﻿using PdfTextReader.Execution;
+using PdfTextReader.PDFText;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace PdfTextReader
+{
+    public class ExamplesAzure
+    {
+        public static void FollowText(string basename)
+        {
+            var pipeline = new Execution.Pipeline();
+
+            pipeline.Input($"{basename}.pdf")
+                    .Output($"{basename}-follow-text-output.pdf")
+                    .AllPages(page => page
+                              .ParsePdf<ProcessPdfText>()
+                              .ShowLine(Color.Orange)
+                    );
+
+            pipeline.Done();
+        }
+    }
+}
