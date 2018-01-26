6fe3f9f49fedd9637443625d38b8667841615c28
Fabricio Catae <fcatae@microsoft.com>
2018-01-26 19:37:10 -0200

    set up Parser Run

---
+++ src/ParserRun/Config.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace ParserRun
+{
+    class Config
+    {
+        private readonly IConfigurationRoot _config;
+
+        public Config(string[] args)
+        {
+            _config = new ConfigurationBuilder()
+                            .AddUserSecrets<Config>(optional: true)
+                            .AddEnvironmentVariables("PDFPARSER")
+                            .AddCommandLine(args)
+                            .Build();
+        }
+
+        [DebuggerHidden]
+        public string Get(string configName)
+        {
+            var value = _config[configName];
+
+            if (value == null)
+                throw new NotConfigured(configName);
+
+            return value;
+        }
+
+        class NotConfigured : Exception
+        {
+            public readonly string Name;
+
+            public NotConfigured(string name) : base($"configuration '{name}' not found")
+            {
+                Name = name;
+            }
+        }
+    }
+}
--- src/ParserRun/Program.cs
+++ src/ParserRun/Program.cs
@@ -6,7 +6,15 @@ namespace ParserRun
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Parser Run");
+
+            // DEV: configure the secrets
+            //
+            //   dotnet user-secrets set exemplo abc
+            //
+            var config = new Config(args);
+
+            Console.WriteLine(config.Get("exemplo1"));
         }
     }
 }
