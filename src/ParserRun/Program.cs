08e5b78cd8ea485f1b11589adb3b43c09d3cc872
Fabricio Catae <fcatae@microsoft.com>
2018-01-26 19:59:25 -0200

    ParserRun access Azure Storage

--- src/ParserRun/Program.cs
+++ src/ParserRun/Program.cs
@@ -8,13 +8,32 @@ namespace ParserRun
         {
             Console.WriteLine("Parser Run");
 
-            // DEV: configure the secrets
-            //
-            //   dotnet user-secrets set exemplo abc
-            //
             var config = new Config(args);
 
-            Console.WriteLine(config.Get("exemplo1"));
+            Console.WriteLine(config.Get("AZURE_STORAGE"));
+            Console.WriteLine(config.Get("AZURE_STORAGE_CONTAINER"));
+
+            // DEV: configure the secrets
+            //
+            //   dotnet user-secrets set AZURE_STORAGE abc
+            //   dotnet user-secrets set AZURE_STORAGE_CONTAINER abc
+            //
+            string connectionString = config.Get("AZURE_STORAGE");
+            string storageContainer = config.Get("AZURE_STORAGE_CONTAINER");
+
+            var blob = new AzureBlob(connectionString, storageContainer);
+            
+            using (var sw = new System.IO.StreamWriter(blob.GetStreamWriter("teste1.txt")))
+            {
+                sw.WriteLine("Hello from WRITER");
+            }
+
+            using (var sr = new System.IO.StreamReader(blob.GetStreamReader("teste1.txt")))
+            {
+                string output = sr.ReadToEnd();
+
+                Console.WriteLine("Output from READER = " + output);
+            }
         }
     }
 }
