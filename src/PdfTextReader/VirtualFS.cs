50f41baa53cfc9a99d4751f0cd0c6ab1412488d6
Fabricio Catae <fcatae@microsoft.com>
2018-01-26 13:53:06 -0200

    add virtual file

---
+++ src/PdfTextReader/VirtualFS.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace PdfTextReader.Base
+{
+    class VirtualFS
+    {
+        public static VirtualFS Storage = new VirtualFS();
+
+        public Stream OpenReader(string filename)
+        {
+            return new FileStream(filename, FileMode.Open);
+        }
+
+        public void OpenWriter(string filename)
+        {
+        }
+    }
+}
