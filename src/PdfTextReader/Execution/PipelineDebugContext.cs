4685828d100bf4a1c7b3093630b05c4b21c3d5c0
Fabricio Catae <fcatae@microsoft.com>
2018-01-26 14:12:35 -0200

    adopt virtual file

--- src/PdfTextReader/Execution/PipelineDebugContext.cs
+++ src/PdfTextReader/Execution/PipelineDebugContext.cs
@@ -15,7 +15,7 @@ namespace PdfTextReader.Execution
 
         public PipelineDebugContext(string filename, string outputname)
         {
-            this._pdf = new PdfDocument(new PdfReader(filename), new PdfWriter(outputname));
+            this._pdf = new PdfDocument(VirtualFS.OpenReader(filename), VirtualFS.OpenWriter(outputname));
         }
 
         public void Dispose()
