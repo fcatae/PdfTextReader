0b80cf6eed73faf977967120391262505badc487
Fabricio Catae <fcatae@microsoft.com>
2018-01-28 17:36:38 -0200

    fix: implement List<T>.TakeLast #66

---
+++ src/PdfTextReader/Compatibility.cs
@@ -0,0 +1,16 @@
+﻿using PdfTextReader.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PdfTextReader
+{
+    static class Compatibility
+    {
+        public static IEnumerable<IBlock> TakeLast(this List<IBlock> list, int end)
+        {
+            int count = list.Count - end;
+            return list.GetRange(end, count);
+        }
+    }
+}
