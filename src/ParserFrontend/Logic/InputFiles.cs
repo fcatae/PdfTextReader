d3dfb2adca56a4d7eb5a6b0bf830d2e127a8d426
Fabricio Catae <fcatae@microsoft.com>
2018-04-11 13:40:45 -0300

    adicionado o arquivo InputFile logic

---
+++ src/ParserFrontend/Logic/InputFiles.cs
@@ -0,0 +1,19 @@
+﻿using PdfTextReader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParserFrontend.Logic
+{
+    public class InputFiles
+    {
+        private readonly IVirtualFS _virtualFS;
+
+        public InputFiles(IVirtualFS virtualFS)
+        {
+            this._virtualFS = virtualFS;
+        }
+
+    }
+}
