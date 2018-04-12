f0ea9abeea61ee31e2cc3a0ecad60d03402a3e00
Fabricio Catae <fcatae@microsoft.com>
2018-04-12 10:39:09 -0300

    add process UI

--- src/ParserFrontend/wwwroot/js/site.js
+++ src/ParserFrontend/wwwroot/js/site.js
@@ -1,1 +1,17 @@
 ﻿// Write your Javascript code.
+$(function () {
+
+
+})
+
+// Upload Page
+$(".upload-form").submit(function (ev) { uploadFormSubmit(); return false; });
+
+function uploadFormSubmit() {
+    var waitImage = "<img src='/images/wait.gif'>";
+
+    // disable all the controls and hide the submit button
+    $(".upload-form input").prop('disabled', true);
+    $(".upload-form input[type='submit']").hide();
+    $(".upload-form .wait-image").show();    
+}
