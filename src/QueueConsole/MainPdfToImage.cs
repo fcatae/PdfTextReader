b27c959f3c5be87f902d834a3705fcd09b2cffc2
Fabricio Catae <fcatae@microsoft.com>
2018-05-17 18:15:10 -0300

    refatoracao do queue

---
+++ src/QueueConsole/MainPdfToImage.cs
@@ -0,0 +1,64 @@
+﻿using PdfTextReader.Azure;
+using QueueConsole.Queue;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueConsole
+{
+    class MainPdfToImage
+    {
+        string _inputStorageAccount;
+        string _queueStorageAccount;
+        string _queueName;
+
+        public void Run(string inputStorageAccount, string queueStorageAccount, string queueName)
+        {
+            _inputStorageAccount = inputStorageAccount;
+            _queueStorageAccount = queueStorageAccount;
+            _queueName = queueName;
+
+            var input = new AzureBlobFileSystem();
+            input.AddStorageAccount("input", inputStorageAccount);
+            input.SetWorkingFolder("wasb://input");
+            var container = input.GetFolder("pdf");
+
+            var itens = container.EnumItems();
+
+            var itens_added = ProcessItensAsync(itens, "pdf").GetAwaiter().GetResult();
+
+            Console.WriteLine($"{itens_added} message(s) added to the queue !");
+
+        }
+
+        private async Task<int> ProcessItensAsync(IEnumerable<IAzureBlob> itens, string fileExtension)
+        {
+            var itens_added = 0;
+
+            var queue = new AzureQueue(_queueStorageAccount, _queueName);
+
+            foreach (var item in itens)
+            {
+                if (item is IAzureBlobFolder)
+                {
+                    var folder = (IAzureBlobFolder)item;
+                    Console.WriteLine($"Folder: {folder.Path}");
+                    itens_added += await ProcessItensAsync(folder.EnumItems(), fileExtension);
+                }
+                else if (item is IAzureBlobFile)
+                {
+                    IAzureBlobFile itemFile = item as IAzureBlobFile;
+                    if (string.Equals(itemFile.Extension, fileExtension, StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        await queue.AddMessageAsync(itemFile.Uri);
+                        Console.WriteLine($"Arquivo: {itemFile.Path}");
+                        itens_added++;
+                    }
+                }
+            }
+
+            return itens_added;
+        }
+    }
+}
--- src/QueueConsole/Program.cs
+++ src/QueueConsole/Program.cs
@@ -14,51 +14,15 @@ namespace QueueConsole
 
         static void Main(string[] args)
         {
-
             Console.WriteLine("Console Queue");
 
             _config = new Config(args);
 
-            var input = new AzureBlobFileSystem();
-            input.AddStorageAccount("input", _config.Get("INPUT_STORAGE_ACCOUNT"));
-            input.SetWorkingFolder("wasb://input");
-            var container = input.GetFolder("pdf");
+            string INPUT_STORAGE_ACCOUNT = _config.Get("INPUT_STORAGE_ACCOUNT");
+            string QUEUE_STORAGE_ACCOUNT = _config.Get("QUEUE_STORAGE_ACCOUNT");
+            string QUEUE_NAME = _config.Get("QUEUE_NAME");
 
-            var itens = container.EnumItems();
-
-            var itens_added = ProcessItensAsync(itens, "pdf").GetAwaiter().GetResult();
-
-            Console.WriteLine($"{itens_added} message(s) added to the queue !");
-
-        }
-
-        private static async Task<int> ProcessItensAsync(IEnumerable<IAzureBlob> itens, string fileExtension)
-        {
-            var itens_added = 0;
-
-            var queue = new AzureQueue(_config.Get("QUEUE_STORAGE_ACCOUNT"), _config.Get("QUEUE_NAME"));
-
-            foreach (var item in itens)
-            {
-                if (item is IAzureBlobFolder)
-                {
-                    var folder = (IAzureBlobFolder)item;
-                    Console.WriteLine($"Folder: {folder.Path}");
-                    itens_added += await ProcessItensAsync(folder.EnumItems(), fileExtension);
-                }
-                else if (item is IAzureBlobFile)
-                {
-                    IAzureBlobFile itemFile = item as IAzureBlobFile;
-                    if (string.Equals(itemFile.Extension, fileExtension, StringComparison.CurrentCultureIgnoreCase))
-                    {
-                        await queue.AddMessageAsync(itemFile.Uri);
-                        Console.WriteLine($"Arquivo: {itemFile.Path}");
-                        itens_added++;
-                    }
-                }
-            }
-
-            return itens_added;
+            (new MainPdfToImage()).Run(INPUT_STORAGE_ACCOUNT, QUEUE_STORAGE_ACCOUNT, QUEUE_NAME);
         }
     }
 }
