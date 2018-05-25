152d66bea8118d35c937f3fe52aba9257bcc3455
Fabricio Catae <fcatae@microsoft.com>
2018-05-25 12:18:53 -0300

    Redimensiona imagem

--- src/WebFrontendImages/Controllers/ImagesController.cs
+++ src/WebFrontendImages/Controllers/ImagesController.cs
@@ -18,7 +18,7 @@ namespace WebFrontendImages.Controllers
         }
 
         [HttpGet]
-        public Task<IActionResult> GetAsync() => GetAsync("DO1_2016_01_06", 1);
+        public Task<IActionResult> GetAsync() => ResizeAsync("DO1_2016_01_06", 1, 0, 0, 50, 50);
 
         // Example:
         // http://localhost/api/images/DO1_2016_01_06/pages/1
@@ -29,6 +29,24 @@ namespace WebFrontendImages.Controllers
             var img = await _source.GetAsync($"{document}/page_{page}.jpg");
 
             return new FileStreamResult(img, "image/jpeg");
+        }
+
+        // Example:
+        // http://localhost/api/images/DO1_2016_01_06/pages/1/resize?x=0&y=0&w=1&h=1
+        //
+        [HttpGet("{document}/pages/{page}/resize")]
+        public async Task<IActionResult> ResizeAsync(string document, int page, [FromQuery]int x, [FromQuery]int y, [FromQuery]int w, [FromQuery]int h)
+        {
+            var img = await _source.GetAsync($"{document}/page_{page}.jpg");
+
+            float tx = x / 100.0F;
+            float ty = y / 100.0F;
+            float tw = w / 100.0F;
+            float th = h / 100.0F;
+
+            var newimg = ImageProcessing.Crop(img, tx, ty, tw, th);
+
+            return new FileStreamResult(newimg, "image/jpeg");
         }
 
         public string GetImage(string document, int page)
---
+++ src/WebFrontendImages/Logic/ImageProcessing.cs
@@ -0,0 +1,43 @@
+﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Jpeg;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Processing.Transforms;
+using SixLabors.Primitives;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebFrontendImages.Logic
+{
+    public class ImageProcessing
+    {
+        static readonly JpegDecoder JPEG = new JpegDecoder();
+
+        public static Stream Crop(Stream stream, float tx, float ty, float tw, float th)
+        {
+            Stream output = new MemoryStream();
+
+            using (Image<Rgba32> image = Image.Load(stream, JPEG))
+            {
+                int x1 = (int)(image.Width * tx);
+                int y1 = (int)(image.Height * ty);
+                int dx = (int)(image.Width * tw);
+                int dy = (int)(image.Height * th);
+
+                image.Mutate(x => x
+                    .Crop(new Rectangle(x1, y1, dx, dy))
+                    //.Resize(image.Width / 2, image.Height / 2)
+                    );
+                                
+                image.Save(output, new JpegEncoder());
+            }
+
+            output.Seek(0, SeekOrigin.Begin);
+
+            return output;
+        }
+    }
+}
