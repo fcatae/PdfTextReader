fddfb117c744e89d8ff338cb5c0bf75c5f91f66e
Fabricio Catae <fcatae@microsoft.com>
2018-05-30 12:43:12 -0300

    Adiciona classe para Zip

---
+++ src/ParserFrontend/Infra/ZipCompression.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO.Compression;
+using System.IO;
+
+namespace ParserFrontend.Infra
+{
+    public class ZipCompression : IDisposable
+    {
+        MemoryStream _memoryStream;
+        ZipArchive _zipArchive;
+
+        public ZipCompression()
+        {
+            _memoryStream = new MemoryStream();
+            _zipArchive = new ZipArchive(_memoryStream, ZipArchiveMode.Create);
+        }
+
+        public void Add(string filename, Stream stream)
+        {
+            if (_zipArchive == null)
+                throw new InvalidOperationException("Object disposed");
+
+            var entry = _zipArchive.CreateEntry(filename);
+
+            using (var destStream = entry.Open())
+            {
+                stream.CopyTo(destStream);
+            }   
+        }
+
+        public void CopyTo(Stream stream)
+        {
+            _memoryStream.CopyTo(stream);
+        }
+
+        public void Dispose()
+        {
+            if(_zipArchive != null)
+            {
+                _zipArchive.Dispose();
+                _zipArchive = null;
+            }
+
+            _memoryStream = null;            
+        }
+    }
+}
