4e87cfbc9d62c2637e49e74c55464b53c0d0c930
Fabricio Catae <fcatae@microsoft.com>
2018-05-30 23:02:38 -0300

    Adiciona controle de calendario

---
+++ src/ParserFrontend/Controllers/BrowserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ParserFrontend.Controllers
+{
+    [Route("browser")]
+    public class BrowserController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("{year}/{tipo}", Name="Browser_YearTipo")]
+        public IActionResult Year(int year, string tipo)
+        {
+            var dict = new Dictionary<string, string> { { "DO1", "Seção 1" }, { "DO2", "Seção 2" }, { "DO3", "Seção 3" } };
+
+            ViewBag.Name = dict[tipo];
+            ViewBag.Year = year;
+
+            return View();
+        }
+    }
+}
